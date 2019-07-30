9dff36f2fde276507215a7fe4f713d59e83b77bf
Max Wofford <max@maxwofford.com>
2019-07-30 12:53:05 -0700

    Initial commit for deploying to Heroku

---
+++ README.md
@@ -0,0 +1,3 @@
+# Orpheus
+
+![](https://raw.githubusercontent.com/hackclub/dinosaurs/master/club_dinosaur.png)
---
+++ index.js
@@ -0,0 +1,35 @@
+const Botkit = require('botkit')
+const Airtable = require('airtable')
+const _ = require('lodash')
+
+const base = new Airtable({apiKey: process.env.AIRTABLE_KEY}).base(process.env.AIRTABLE_BASE);
+
+const redisConfig = {
+  url: process.env.REDISCLOUD_URL
+}
+const redisStorage = require('botkit-storage-redis')(redisConfig)
+
+console.log("reticulating splines...")
+console.log("booting dinosaur...")
+
+const bot = Botkit.slackbot({
+  clientId: process.env.SLACK_CLIENT_ID,
+  clientSecret: process.env.SLACK_CLIENT_SECRET,
+  clientSigningSecret: process.env.SLACK_CLIENT_SIGNING_SECRET,
+  scopes: ['bot', 'chat:write:bot'],
+  storage: redisStorage
+});
+
+controller.setupWebserver(process.env.PORT, function(err,webserver) {
+  controller.createWebhookEndpoints(controller.webserver)
+  controller.createOauthEndpoints(controller.webserver)
+});
+
+controller.hears('.*', 'direct_message,direct_mention', (bot, message) => {
+  const { text, user } = message
+
+  // ignore threaded messages
+  if (_.has(message.event, 'parent_user_id')) return
+
+  bot.replyInThread(message, 'Not sure what that means...')
+})
