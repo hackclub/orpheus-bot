e28953e158ecfa9df6f6b667136eafe1932ad65d
Max Wofford <max@maxwofford.com>
2019-08-05 15:12:32 -0700

    revert bad idea

--- interactions/checkin.js
+++ interactions/checkin.js
@@ -1,6 +1,4 @@
-const {
-  getInfoForUser
-} = require('../utils.js')
+const { getInfoForUser } = require('../utils.js')
 
 const interactionCheckin = (bot, message) => {
   bot.startConversation(message, (err, convo) => {
@@ -28,75 +26,72 @@ const interactionCheckin = (bot, message) => {
           text: `I don't have any record of you being a club leader (ಠ_ಠ)`
         })
         convo.stop()
-      } else {
-        dataAvailableConvo(leader, club, history)
-        convo.gotoThread('found')
+        return
       }
+      convo.addMessage({
+        delay: 2000,
+        text: `Found you! It's *${leader.fields['Full Name']}*, right?`
+      }, 'found')
+      convo.addMessage({
+        delay: 2000,
+        text: `From ${club.fields['Name']}`,
+        action: 'date'
+      }, 'found')
+
+      convo.addMessage('What day was it on?', 'date')
+      convo.addQuestion({
+        text: 'When was your meeting?',
+        blocks: [{
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "(You can tell me a date in `YYYY-MM-DD` format, or click a shortcut button"
+            }
+          },
+          {
+            "type": "actions",
+            "elements": [{
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "text": `Today (${new Date(Date.now()).toLocaleDateString('en-us', { weekday: 'long' })})`
+              },
+              "value": "today"
+            }]
+          }
+        ]
+      }, [{
+          pattern: 'today',
+          callback: (response, convo) => {
+            console.log('*User met today*')
+            convo.setVar('date', 'today')
+            bot.replyInteractive(response, '_You tell orpheus you met today_')
+            convo.say(`Ok, I'll record that you met today, *${new Date(Date.now()).toLocaleDateString()}*`)
+            convo.gotoThread('attendance')
+          }
+        },
+        {
+          default: true,
+          callback: (response, convo) => {
+            console.log(response, convo)
+            convo.repeat()
+          }
+        }
+      ], {}, 'date')
+
+      convo.addQuestion(`How many people showed up? (please just enter digits– I'm fragile)`, (response, convo) => {
+        const attendance = +response.text
+        console.log('attendance:', attendance)
+        console.log('response', response)
+        console.log('convo', convo)
+
+        convo.say(`I parsed that as *${attendance}* hackers`)
+        convo.gotoThread('')
+      }, {}, 'attendance')
+
+      convo.gotoThread('found')
     })
   })
-}
-
-const dataAvailableConvo = (leader, club, history) => {
-  convo.addMessage({
-    delay: 2000,
-    text: `Found you! It's *${leader.fields['Full Name']}*, right?`
-  }, 'found')
-  convo.addMessage({
-    delay: 2000,
-    text: `From ${club.fields['Name']}`,
-    action: 'date'
-  }, 'found')
-
-  convo.addMessage('What day was it on?', 'date')
-  convo.addQuestion({
-    text: 'When was your meeting?',
-    blocks: [{
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "(You can tell me a date in `YYYY-MM-DD` format, or click a shortcut button"
-        }
-      },
-      {
-        "type": "actions",
-        "elements": [{
-          "type": "button",
-          "text": {
-            "type": "plain_text",
-            "text": `Today (${new Date(Date.now()).toLocaleDateString('en-us', { weekday: 'long' })})`
-          },
-          "value": "today"
-        }]
-      }
-    ]
-  }, [{
-      pattern: 'today',
-      callback: (response, convo) => {
-        console.log('*User met today*')
-        convo.setVar('date', 'today')
-        bot.replyInteractive(response, '_You tell orpheus you met today_')
-        convo.say(`Ok, I'll record that you met today, *${new Date(Date.now()).toLocaleDateString()}*`)
-        convo.gotoThread('attendance')
-      }
-    },
-    {
-      default: true,
-      callback: (response, convo) => {
-        console.log(response, convo)
-        convo.repeat()
-      }
-    }
-  ], {}, 'date')
-
-  convo.addQuestion(`How many people showed up? (please just enter digits– I'm fragile)`, (response, convo) => {
-    const attendance = +response.text
-    console.log('attendance:', attendance)
-    console.log('response', response)
-    console.log('convo', convo)
-
-    convo.say(`I parsed that as *${attendance}* hackers`)
-    convo.gotoThread('')
-  }, {}, 'attendance')
 }
 
 module.exports = interactionCheckin
