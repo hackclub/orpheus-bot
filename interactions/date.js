346af0b35b715baa758899078fff219aded39926
Max Wofford <max@maxwofford.com>
2019-08-06 15:33:56 -0700

    fix order of commands

--- interactions/date.js
+++ interactions/date.js
@@ -1,9 +1,9 @@
 const chrono = require('chrono-node')
 
 const interactionDate = (bot, message) => {
-  console.log(`"${message.text}" parsed as ${meetingDate}`)
   // bot.api.users.info({ user: message.user }, (err, res) => {
     const meetingDate = chrono.parse(message.text)
+    console.log(`"${message.text}" parsed as ${meetingDate}`)
     bot.reply(message, `I parsed that as ${meetingDate}`)
   // }
 }
