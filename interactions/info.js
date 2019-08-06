864517c332f3dd6fab16ac3939509094c06c7f8b
Max Wofford <max@maxwofford.com>
2019-08-06 14:22:49 -0700

    Attempt to get user info

--- interactions/info.js
+++ interactions/info.js
@@ -1,6 +1,8 @@
 const interactionInfo = (bot, message) => {
   const ts = process.env.STARTUP_TIME
   bot.reply(message, `_Dino started at ${ts} (${Date.now() - ts} milliseconds since last incident)_`)
+
+  console.log('user timezone detected as', bot.api.users.info({ user: message.user }))
 }
 
 module.exports = interactionInfo
