0a75db096aefbd73fc0ba352a93c3c1c2e7694a8
Max Wofford <max@maxwofford.com>
2019-08-05 22:28:06 -0700

    attempt to fix info message

--- interactions/info.js
+++ interactions/info.js
@@ -1,6 +1,6 @@
 const interactionInfo = (bot, message) => {
   const ts = process.env.STARTUP_TIME
-  bot.reply(`_Dino started at ${ts} (${Date.now() - ts} milliseconds since last incident)_`)
+  bot.reply(message, `_Dino started at ${ts} (${Date.now() - ts} milliseconds since last incident)_`)
 }
 
 module.exports = interactionInfo
