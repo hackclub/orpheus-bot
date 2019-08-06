3b85837b0027dd301850206dfb3d48cee1845d45
Max Wofford <max@maxwofford.com>
2019-08-06 15:26:58 -0700

    Add console message to date command

--- interactions/date.js
+++ interactions/date.js
@@ -1,6 +1,7 @@
 const chrono = require('chrono')
 
 const interactionDate = (bot, message) => {
+  console.log(`"${message.text}" parsed as ${meetingDate}`)
   // bot.api.users.info({ user: message.user }, (err, res) => {
     const meetingDate = chrono.parse(message.text)
     bot.reply(message, `I parsed that as ${meetingDate}`)
