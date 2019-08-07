419cb556db75b697fae1c646e78c29c1bd321782
Max Wofford <max@maxwofford.com>
2019-08-06 18:11:18 -0700

    info command shows minutes of uptime

--- src/interactions/info.js
+++ src/interactions/info.js
@@ -1,6 +1,6 @@
 const interactionInfo = (bot, message) => {
   const ts = process.env.STARTUP_TIME
-  bot.reply(message, `_Dino started at ${ts} (${Date.now() - ts} milliseconds since last incident)_`)
+  bot.reply(message, `_Dino started at ${ts} (${Math.round((Date.now() - ts) / 100 / 60)} minutes since last incident)_`)
   bot.api.users.info({ user: message.user }, (err, res) => {
     if (err) console.error(err)
     const user = res.user
