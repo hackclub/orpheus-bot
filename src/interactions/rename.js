aea59ddf0b13f852245368ecfa10990bb7382c49
Max Wofford <max@maxwofford.com>
2019-08-08 12:51:06 -0700

    Add ephemeral responses to /rename-channel

--- src/interactions/rename.js
+++ src/interactions/rename.js
@@ -1,28 +1,18 @@
 import { airFind } from '../utils'
 
 const renameInteraction = (bot, message) => {
-  const { user, channel } = message
+  const { user, channel, text } = message
 
-  const callerInfo = {
-    clubRecord: null,
-    leaderRecord: null
-  }
-
-  Promise.all([
-    airFind('Leaders', 'Slack ID', user).then(l => callerInfo.leaderRecord = l),
-    airFind('Clubs', 'Slack Channel ID', channel).then(c => callerInfo.leaderRecord = c)
-  ]).then(() => {
-    if (!callerInfo.leaderRecord) {
-      
+  airFind('Clubs', 'Slack Channel ID', channel).then(club => {
+    if (!club) {
+      // not a club channel
+      bot.whisper(message, `This command only works on club channels.`)
+    } else if (club.fields['Leader Slack IDs'].indexOf(user) === -1) {
+      // user not found in club's list of leaders
+      bot.whisper(message, `Only this club's leaders can run this command. You aren't marked as this club's leader.`)
+    } else {
+      bot.whisper(message, `Got it! Renaming the channel to ${text}`)
     }
-
-    if (!callerInfo.clubRecord) {
-
-    }
-
-    console.log(callerInfo.leaderRecord.fields)
-    console.log(callerInfo.clubRecord.fields)
-
   })
 }
 export default renameInteraction
