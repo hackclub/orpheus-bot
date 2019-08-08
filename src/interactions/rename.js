0fd2f57dafb37655d336b56bdcb651ef26f0f2d9
Max Wofford <max@maxwofford.com>
2019-08-08 12:56:07 -0700

    Add channels.rename api call to rename command

--- src/interactions/rename.js
+++ src/interactions/rename.js
@@ -11,7 +11,13 @@ const renameInteraction = (bot, message) => {
       // user not found in club's list of leaders
       bot.whisper(message, `Only this club's leaders can run this command. You aren't marked as this club's leader.`)
     } else {
-      bot.whisper(message, `Got it! Renaming the channel to ${text}`)
+      bot.whisper(message, `You got it boss! Renaming the channel to "${text}"...`)
+      const renamedChannel = bot.api.channels.rename({
+        channel,
+        name: text,
+        validate: true
+      })
+      console.log(renamedChannel)
     }
   })
 }
