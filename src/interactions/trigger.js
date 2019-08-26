678f9ed7ab18a66c2770f7972432728cf09ab1a0
Max Wofford <max@maxwofford.com>
2019-08-26 07:53:54 -0400

    Remove old heartbeat promise

--- src/interactions/trigger.js
+++ src/interactions/trigger.js
@@ -44,47 +44,6 @@ const triggerInteraction = (bot, message) => {
     console.error(err)
     bot.whisper(message, `Got error: \`${err}\``)
   })
-
-  // new Promise((resolve, reject) => {
-  //   bot.api.users.info(message, (err, res) => {
-  //     console.log(err, res)
-  //     if (err) { reject(err) }
-  //     const isAuthed = res.user.is_admin || res.user.is_owner
-  //     resolve(isAuthed)
-  //   })
-  // }).then((isAuthed) => {
-  //   // ensure posted by admins
-  //   if (!isAuthed) {
-  //     bot.api.reactions.add({
-  //       timestamp: message.ts,
-  //       channel: message.channel,
-  //       name: 'broken_heart'
-  //     })
-  //     return
-  //   }
-
-  //   bot.api.reactions.add({
-  //     timestamp: message.ts,
-  //     channel: message.channel,
-  //     name: 'heartbeat'
-  //   })
-    
-  //   getAllClubs().then(clubs => clubs.forEach(club => {
-  //     const day = club.fields['Checkin Day']
-  //     const hour = club.fields['Checkin Hour']
-  //     const channel = club.fields['Slack Channel ID']
-
-  //     if (!day) { return }
-  //     if (!hour) { return }
-  //     if (!channel) { return }
-
-  //     console.log(`*starting checkin w/ "${club.fields['ID']}" in channel ${channel}*`)
-  //     // TODO: Trigger a check-in from here
-  //   }))
-  // }).catch(err => {
-  //   console.error(err)
-  //   bot.whisper(message, `Got error: \`${err}\``)
-  // })
 }
 
 export default triggerInteraction
