7f599f3aff2f7dee20e9733b4ed751ab4d5707e6
Max Wofford <max@maxwofford.com>
2019-08-26 11:15:18 -0400

    Add timezone to meeting add command

--- src/interactions/meetingAdd.js
+++ src/interactions/meetingAdd.js
@@ -1,86 +1,98 @@
 import { parseDate } from 'chrono-node'
 
-const meetingAdd = (bot, message) => {
-  bot.whisper(message, { blocks: [
-    {
-      type: 'context',
-      elements: [ {
-        type: 'mrkdwn',
-        text: `/meeting-add ${message.text}`
-      } ]
+const getTz = (bot, user) => new Promise((resolve, reject) => {
+  bot.api.users.info({ user }, (err, res) => {
+    if (err) {
+      console.error(err)
+      reject(err)
     }
-  ]})
+    resolve(res.user.tz)
+  })
+})
 
-  if (message.text.indexOf(',') === -1) {
-    // either the user typed "help" or an incorrectly formatted command
-    const manualMsg = "_Placeholder usage instructions_"
-    bot.whisper(message, manualMsg, (err, response) => {
+const meetingAdd = (bot, message) => {
+  getTz(bot, message.user).then(timeZone => {
+    bot.whisper(message, { blocks: [
+      {
+        type: 'context',
+        elements: [ {
+          type: 'mrkdwn',
+          text: `/meeting-add ${message.text}`
+        } ]
+      }
+    ]})
+
+    if (message.text.indexOf(',') === -1) {
+      // either the user typed "help" or an incorrectly formatted command
+      const manualMsg = "_Placeholder usage instructions_"
+      bot.whisper(message, manualMsg, (err, response) => {
+        if (err) {console.error(err)}
+      })
+      return
+    }
+
+    const [ rawDate, rawAttendance, ...other ] = message.text.split(',')
+    const date = parseDate(rawDate)
+    const dayName = date.toLocaleDateString('en-us', { weekday: 'long', timeZone })
+    const mmddyyyy = date.toLocaleDateString('en-us', {timeZone})
+    const attendance = parseInt((rawAttendance.match(/(\d+)/) || [])[0])
+
+    bot.whisper(message, { blocks: [
+      {  
+        "type":"section",
+        "text":{  
+          "type":"mrkdwn",
+          "text":"Just to confirm, is the following correct?"
+        }
+      },
+      {  
+        "type":"divider"
+      },
+      {  
+        "type":"section",
+        "text":{  
+          "type":"mrkdwn",
+          "text":`Date: *${dayName} (${mmddyyyy})}*\nAttendance: *${attendance}*`
+        }
+      },
+      {  
+        "type":"divider"
+      },
+      {  
+        "type":"actions",
+        "elements":[  
+          {  
+            "type":"button",
+            "text":{  
+              "type":"plain_text",
+              "text":"✅ submit",
+              "emoji":true
+            },
+            "value":"true"
+          },
+          {  
+            "type":"button",
+            "text":{  
+              "type":"plain_text",
+              "text":"⛔️ cancel",
+              "emoji":true
+            },
+            "value":"false"
+          }
+        ]
+      },
+      {  
+        "type":"context",
+        "elements":[  
+          {  
+            "type":"mrkdwn",
+            "text":"For help, type `/meeting-add help`"
+          }
+        ]
+      }
+    ]}, (err, response) => {
       if (err) {console.error(err)}
     })
-    return
-  }
-
-  const [ rawDate, rawAttendance, ...other ] = message.text.split(',')
-  const date = parseDate(rawDate)
-  const dayName = date.toLocaleDateString('en-us', { weekday: 'long', timeZone })
-  const mmddyyyy = date.toLocaleDateString('en-us', {timeZone})
-  const attendance = parseInt((rawAttendance.match(/(\d+)/) || [])[0])
-
-  bot.whisper(message, { blocks: [
-    {  
-      "type":"section",
-      "text":{  
-        "type":"mrkdwn",
-        "text":"Just to confirm, is the following correct?"
-      }
-    },
-    {  
-      "type":"divider"
-    },
-    {  
-      "type":"section",
-      "text":{  
-        "type":"mrkdwn",
-        "text":`Date: *${dayName (mmddyyyy)}*\nAttendance: *${attendance}*`
-      }
-    },
-    {  
-      "type":"divider"
-    },
-    {  
-      "type":"actions",
-      "elements":[  
-        {  
-          "type":"button",
-          "text":{  
-            "type":"plain_text",
-            "text":"✅ submit",
-            "emoji":true
-          },
-          "value":"true"
-        },
-        {  
-          "type":"button",
-          "text":{  
-            "type":"plain_text",
-            "text":"⛔️ cancel",
-            "emoji":true
-          },
-          "value":"false"
-        }
-      ]
-    },
-    {  
-      "type":"context",
-      "elements":[  
-        {  
-          "type":"mrkdwn",
-          "text":"For help, type `/meeting-add help`"
-        }
-      ]
-    }
-  ]}, (err, response) => {
-    if (err) {console.error(err)}
   })
 }
 export default meetingAdd
