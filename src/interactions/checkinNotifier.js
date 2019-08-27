e1da5307748c6ecf08fe73ea8139445ed5e69b89
Max Wofford <max@maxwofford.com>
2019-08-27 11:56:09 -0400

    Attempt to fix notifier not posting in channel

--- src/interactions/checkinNotifier.js
+++ src/interactions/checkinNotifier.js
@@ -1,9 +1,12 @@
 import { initBot } from "../utils";
 
 const checkinNotifierInteraction = (bot=initBot(), message) => {
-  console.log(bot)
   const text = "Hey! My calendar shows you had a meeting recently. If you did you can react to this message with an emoji to let me know."
-  bot.say({text, message})
+
+  bot.say({
+    text,
+    channel: message.channel
+  })
 }
 
 export default checkinNotifierInteraction
