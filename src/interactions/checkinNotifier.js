33d614ac115185c5ae32398c4a318dd08c159b3d
Max Wofford <max@maxwofford.com>
2019-08-27 15:58:56 -0400

    Remove unused import

--- src/interactions/checkinNotifier.js
+++ src/interactions/checkinNotifier.js
@@ -1,5 +1,4 @@
 import { initBot } from "../utils";
-import interactionCheckin from "./checkin";
 
 const checkinNotifierInteraction = (bot=initBot(), message) => {
   const text = "Hey! My calendar shows you had a meeting recently. If you did you can react to this message with an emoji to let me know."
