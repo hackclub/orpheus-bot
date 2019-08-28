aaa4cf744d7c3db4b7e5df4af77cdd05064df192
Max Wofford <max@maxwofford.com>
2019-08-28 17:46:32 -0400

    Don't overwrite first meeting time

--- src/interactions/meetingTime.js
+++ src/interactions/meetingTime.js
@@ -5,24 +5,27 @@ const interactionMeetingTime = (bot, message) => {
   getInfoForUser(message.user).then(({ leader, club, history }) => {
     const currDay = club.fields['Checkin Day']
     const currHour = club.fields['Checkin Hour']
-
+    
     const inputDate = parseDate(message.text)
-
-
+    
+    
     if (inputDate) {
       const updatedFields = {}
       updatedFields['Checkin Day'] = 'Monday'
       updatedFields['Checkin Hour'] = '2'
-      if (!club.fields['legacy']) { updatedFields['First Meeting Time'] = Date.now() }
+      if (!club.fields['legacy'] && club.fields['First Meeting Time']) {
+        updatedFields['First Meeting Time'] = inputDate
+      }
+
       airPatch('Clubs', club.id, updatedFields).then(record => {
         bot.whisper(message, `Ok, I'll post a message in your club's channel at ${record.fields['Checkin Hour']}:00 on ${record.fields['Checkin Day']}`)
       }).catch(err => {
         bot.whisper(message, `Got error: \`${err}\``)
       })
-
+      
     } else {
       bot.whisper(message, `Use this command to record when your meetings will occur each week. Ex. \`/meeting-time next Tuesday at 3:30 PM\``)
-
+      
       if (!currDay || !currHour) {
         bot.whisper(message, `_Currently, ${club.fields['Name']} doesn't have a meeting time set_`)
       } else {
