7a70fa484f58d12370641ce1383f96817a4fb060
Max Wofford <max@maxwofford.com>
2019-08-29 11:08:30 -0400

    Update date.js

--- src/interactions/date.js
+++ src/interactions/date.js
@@ -4,6 +4,7 @@ import { getInfoForUser } from '../utils'
 const interactionDate = (bot, message) => {
   getInfoForUser(message.user).then(({ slackUser }) => {
     const meetingDate = parseDate(message.text + ' ' + slackUser.tz_label)
+    console.log(message.text, slackUser.tz_label)
     bot.reply(message, `I parsed input ${message.text} as ${meetingDate}`)
   })
 }
