277de49561ba7e4d90c8c170e99ce3e34a00c8c9
Max Wofford <max@maxwofford.com>
2019-09-06 10:12:15 -0400

    Remove depreciated api call from info command

--- src/interactions/info.js
+++ src/interactions/info.js
@@ -6,14 +6,6 @@ const interactionInfo = (bot, message) => {
       (Date.now() - ts) / 1000 / 60
     )} minutes since last incident)_`
   )
-  bot.api.apps.permissions.scopes.list((err, res) => {
-    if (err) { console.error(err) }
-    bot.reply(message, `
-\`\`\`
-${res.scopes}
-\`\`\`
-    `)
-  })
   bot.api.users.info({ user: message.user }, (err, res) => {
     if (err) console.error(err)
     const user = res.user
