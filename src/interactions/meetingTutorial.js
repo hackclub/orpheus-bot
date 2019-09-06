ad34b5cab91b3e0a9dfcad881d1a94a31023a8c7
Max Wofford <max@maxwofford.com>
2019-09-06 11:05:07 -0400

    Update meetingTutorial.js

--- src/interactions/meetingTutorial.js
+++ src/interactions/meetingTutorial.js
@@ -2,6 +2,7 @@ import { airRecord } from '../utils'
 
 const interactionMeetingTutorial = (bot, message) => {
   const { user } = message
+  console.log('Running meeting tutorial')
 
   // bot.whisper(message, `Hey <@${user}>! Welcome to the check-in tutorial. First I'll need to know when your first meeting is. Run this command to let me know: \`/meeting-time next wednesday at 4 PM\``)
   // bot.whisper(message, "(If you don't know when your first meeting will be, just set it for a couple weeks for now so we can get through the tutorial, then you can change it later)")
