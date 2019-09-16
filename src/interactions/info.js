f370481690c7dd7c6a848484681eb5ab8f20ba74
Max Wofford <max@maxwofford.com>
2019-09-16 10:33:43 -0400

    Remove unused api call in info

--- src/interactions/info.js
+++ src/interactions/info.js
@@ -1,17 +1,12 @@
 const interactionInfo = (bot, message) => {
   const ts = process.env.STARTUP_TIME
+
   bot.reply(
     message,
     `_Dino started at ${ts} (${Math.round(
       (Date.now() - ts) / 1000 / 60
     )} minutes since last incident)_`
   )
-  bot.api.users.info({ user: message.user }, (err, res) => {
-    if (err) console.error(err)
-    const user = res.user
-
-    console.log(user)
-  })
 }
 
 export default interactionInfo
