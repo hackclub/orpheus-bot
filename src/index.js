52205969b9313d9cdeae8a8f7437159759751c7a
Max Wofford <max@maxwofford.com>
2019-09-16 10:32:47 -0400

    Remove unused convo

--- src/index.js
+++ src/index.js
@@ -190,12 +190,6 @@ controller.hears('checkin', 'direct_message,direct_mention', (bot, message) => {
 
 controller.hears('thump', 'ambient', interactionTrigger)
 
-controller.hears('convo', 'direct_mention,direct_message', (bot, message) => {
-  bot.startPrivateConversation(message, (err, convo) => {
-    convo.say('hello!')
-  })
-})
-
 controller.hears('date', 'direct_mention', interactionDate)
 
 controller.hears('info', 'direct_message,direct_mention', interactionInfo)
