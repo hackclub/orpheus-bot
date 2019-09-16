41fd3b920f55a91a6736a029794f80eb2e8f3117
Max Wofford <max@maxwofford.com>
2019-09-16 12:50:02 -0400

    Update copy of rename-channel

--- src/interactions/rename.js
+++ src/interactions/rename.js
@@ -1,61 +1,52 @@
-import { airFind, initBot } from '../utils'
+import { initBot, text as transcript, getInfoForUser } from '../utils'
 
 const interactionRename = (bot, message) => {
   const { user, channel, text } = message
 
-  if (text === '') {
-    bot.whisper(
-      message,
-      "Use this command to rename your club's channel. For example: `/rename-channel el-segundo-high-school`"
-    )
+  if (text === '' || text === 'help') {
+    console.log(`I responded to ${user} with a help message`)
+    bot.whisper(message, transcript('renameChannel.help'))
     return
   }
 
-  airFind('Clubs', 'Slack Channel ID', channel)
-    .then(club => {
-      if (!club || !club.fields) {
-        console.log('*no club or fields set*')
-        bot.whisper(message, `This command only works on club channels.`)
-      } else if (club.fields['Leader Slack IDs'].indexOf(user) === -1) {
-        console.log("*user isn't a leader*")
-        bot.whisper(
-          message,
-          `Only this club's leaders can run this command. You aren't marked as this club's leader.`
-        )
-      } else {
-        console.log(`*Renaming the channel to "${text}*`)
-        initBot(true).api.conversations.rename(
-          {
-            channel,
-            name: text.toLowerCase(),
-          },
-          (err, res) => {
-            if (err) {
-              console.error(err)
-              bot.whisper(
-                message,
-                `hmm... something went wrong. I tried saving "${text}" but it says \`${err}\` in red`
-              )
-              return
-            }
+  getInfoForUser(user).then(({ leader, club }) => {
+    if (!leader) {
+      console.log(`${user} isn't a leader, so I told them this was restricted`)
+      bot.whisper(message, transcript('renameChannel.invalidUser'))
+      return
+    }
 
-            bot.whisper(
-              message,
-              `You got it boss! Renaming the channel to "${text}"...`
-            )
+    if (!club) {
+      console.log(`${user} didn't run this in a club channel`)
+      bot.whisper(message, transcript('renameChannel.noClubChannel'))
+      return
+    }
 
-            setTimeout(() => {
-              bot.whisper(
-                message,
-                `I don't have permission to change channel names directly, but I've got <@U0C7B14Q3>'s password (he stashes them under his mattress), so I logged in as him to rename it.`
-              )
-            }, 2000)
-          }
-        )
+    if (club.fields['Slack Channel ID'] != channel) {
+      console.log(
+        `${channel} isn't ${user}'s channel, so I asked them to run it there`
+      )
+      bot.whisper(message, transcript('invalidChannel', { channel }))
+      return
+    }
+
+    console.log(`*Renaming the channel to "${text}*`)
+    initBot(true).api.conversations.rename(
+      { channel, name: text.toLowerCase() },
+      err => {
+        if (err) {
+          console.error(err)
+          bot.whisper(message, transcript('renameChannel.error', { text, err }))
+          return
+        }
+
+        bot.whisper(message, transcript('renameChannel.progress', { text }))
+
+        setTimeout(() => {
+          bot.whisper(message, transcript('renameChannel.success'))
+        }, 2000)
       }
-    })
-    .catch(err => {
-      console.error(err)
-    })
+    )
+  })
 }
 export default interactionRename
