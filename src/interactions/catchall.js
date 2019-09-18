01d919e7096270369cd409091c8f53cd373b96fd
Max Wofford <max@maxwofford.com>
2019-09-18 12:53:31 -0400

    Prettier on catchall.js

--- src/interactions/catchall.js
+++ src/interactions/catchall.js
@@ -1,23 +1,21 @@
-import {
-  text
-} from '../utils'
+import { text } from '../utils'
 
 const interactionCatchall = (bot, message) => {
-  const {
-    ts: timestamp,
-    channel,
-  } = message
+  const { ts: timestamp, channel } = message
 
   if (Math.random() > 0.5) {
     bot.replyInThread(message, text('catchall.reply'))
   } else {
-    bot.api.reactions.add({
-      timestamp,
-      channel,
-      name: text('catchall.emoji'),
-    }, (err) => {
-      if (err) console.error(err)
-    })
+    bot.api.reactions.add(
+      {
+        timestamp,
+        channel,
+        name: text('catchall.emoji'),
+      },
+      err => {
+        if (err) console.error(err)
+      }
+    )
   }
 }
 
