bfc2ccd5cbc1a9d7feb36052162f49dcb1b41729
Max Wofford <max@maxwofford.com>
2019-09-20 01:58:27 -0400

    catchall responds differently to bots

--- src/interactions/catchall.js
+++ src/interactions/catchall.js
@@ -1,22 +1,24 @@
-import { text } from '../utils'
+import { text, getInfoForUser } from '../utils'
 
 const interactionCatchall = (bot, message) => {
-  const { ts: timestamp, channel } = message
+  const { ts: timestamp, channel, user } = message
 
-  if (Math.random() > 0.5) {
-    bot.replyInThread(message, text('catchall.reply'))
-  } else {
-    bot.api.reactions.add(
-      {
-        timestamp,
-        channel,
-        name: text('catchall.emoji'),
-      },
-      err => {
-        if (err) console.error(err)
+  getInfoForUser(user).then(({ slackUser }) => {
+    if (slackUser.is_bot) {
+      bot.replyInThread(message, text('catchall.botReply'))
+    } else {
+      if (Math.random() > 0.5) {
+        bot.replyInThread(message, text('catchall.reply'))
+      } else {
+        bot.api.reactions.add(
+          { timestamp, channel, name: text('catchall.emoji') },
+          err => {
+            if (err) console.error(err)
+          }
+        )
       }
-    )
-  }
+    }
+  })
 }
 
 export default interactionCatchall
