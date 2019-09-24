e2e679c4e660ccf52c059ed3e6d45b71133b72fc
Max Wofford <max@maxwofford.com>
2019-09-24 14:09:51 -0400

    Attempt to fix /stats command running in uninvited channels

--- src/interactions/stats.js
+++ src/interactions/stats.js
@@ -1,71 +1,152 @@
 import _ from 'lodash'
-import { getInfoForUser, text, initBot } from '../utils'
+import { getInfoForUser, text } from '../utils'
 
-const interactionStats = (_bot, message) => {
-  const bot = initBot()
+const interactionStats = (bot, message) => {
   const { user } = message
 
-  bot.replyAndUpdate(
-    message,
-    text('stats.loadingMessage'),
-    (err, src, updateResponse) => {
-      if (err) console.error(err)
-      getInfoForUser(user)
-        .then(({ leader, club, history }) => {
-          setTimeout(() => {
-            if (!leader || !club) {
-              updateResponse(text('stats.notFound'))
-              return
-            }
-            if (!history || !history.meetings) {
-              updateResponse(text('stats.noMeeting'))
-              return
-            }
+  const loaderPromise = new Promise((resolve, reject) => {
+    bot.replyPrivateDelayed(
+      message,
+      text('stats.loadingMessage'),
+      (err, res) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(res)
+        }
+      }
+    )
+  })
 
-            const content = {
-              blocks: [
-                {
-                  type: 'section',
-                  text: {
-                    type: 'mrkdwn',
-                    text: `Stats for *${club.fields['Name']}*`,
-                  },
-                },
-                {
-                  type: 'divider',
-                },
-                {
-                  type: 'image',
-                  title: {
-                    type: 'plain_text',
-                    text: 'attendance',
-                  },
-                  image_url: graphUrl(history, club),
-                  alt_text: 'attendance',
-                },
-                {
-                  type: 'context',
-                  elements: [
-                    {
-                      type: 'mrkdwn',
-                      text: `Requested by <@${message.user}>`,
-                    },
-                  ],
-                },
-              ],
-            }
-            console.log(graphUrl(history, club))
-            updateResponse(content, err => {
-              if (err) console.error(err)
-            })
-          }, 2000)
-        })
-        .catch(err => {
-          updateResponse(text('stats.error', { err }))
-          console.error(err)
-        })
-    }
-  )
+  const minWaitPromise = new Promise(resolve => {
+    setTimeout(resolve, 2000)
+  })
+
+  const infoPromise = getInfoForUser(user)
+
+  Promise.all([loaderPromise, infoPromise, minWaitPromise])
+    .then((res, info, _timeout) => {
+      const { leader, club, history } = info
+
+      if (!leader || !club) {
+        bot.replyPrivateDelayed(res, text('stats.notFound'))
+        return
+      }
+      if (!history || !history.meetings) {
+        bot.replyPrivateDelayed(res, text('stats.noMeeting'))
+        return
+      }
+
+      const content = {
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: `Stats for *${club.fields['Name']}*`,
+            },
+          },
+          {
+            type: 'divider',
+          },
+          {
+            type: 'image',
+            title: {
+              type: 'plain_text',
+              text: 'attendance',
+            },
+            image_url: graphUrl(history, club),
+            alt_text: 'attendance',
+          },
+          {
+            type: 'context',
+            elements: [
+              {
+                type: 'mrkdwn',
+                text: `Requested by <@${message.user}>`,
+              },
+            ],
+          },
+        ],
+      }
+      console.log(graphUrl(history, club))
+      bot.replyPrivateDelayed(res, content, err => {
+        if (err) throw err
+      })
+    })
+    .catch(err => {
+      console.error(err)
+      bot.replyPrivateDelayed(res, text('errors.general', { err }))
+    })
+
+  // try {
+  //   bot.replyAndUpdate(
+  //     message,
+  //     text('stats.loadingMessage'),
+  //     (err, src, updateResponse) => {
+  //       if (err) {
+  //         throw err
+  //       }
+  //       getInfoForUser(user)
+  //         .then(({ leader, club, history }) => {
+  //           setTimeout(() => {
+  //             if (!leader || !club) {
+  //               updateResponse(text('stats.notFound'))
+  //               return
+  //             }
+  //             if (!history || !history.meetings) {
+  //               updateResponse(text('stats.noMeeting'))
+  //               return
+  //             }
+
+  //             const content = {
+  //               blocks: [
+  //                 {
+  //                   type: 'section',
+  //                   text: {
+  //                     type: 'mrkdwn',
+  //                     text: `Stats for *${club.fields['Name']}*`,
+  //                   },
+  //                 },
+  //                 {
+  //                   type: 'divider',
+  //                 },
+  //                 {
+  //                   type: 'image',
+  //                   title: {
+  //                     type: 'plain_text',
+  //                     text: 'attendance',
+  //                   },
+  //                   image_url: graphUrl(history, club),
+  //                   alt_text: 'attendance',
+  //                 },
+  //                 {
+  //                   type: 'context',
+  //                   elements: [
+  //                     {
+  //                       type: 'mrkdwn',
+  //                       text: `Requested by <@${message.user}>`,
+  //                     },
+  //                   ],
+  //                 },
+  //               ],
+  //             }
+  //             console.log(graphUrl(history, club))
+  //             updateResponse(content, err => {
+  //               if (err) console.error(err)
+  //             })
+  //           }, 2000)
+  //         })
+  //         .catch(err => {
+  //           updateResponse(text('stats.error', { err }))
+  //           throw { err }
+  //         })
+  //     }
+  //   )
+  // } catch (err) {
+  //   console.error(err)
+  //   bot.whisper(message, text('errors.general', { err }))
+  // }
 }
 
 const graphUrl = (history, club) => {
