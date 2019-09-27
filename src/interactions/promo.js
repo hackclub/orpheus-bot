bedfb35d1c22e136c1f00934c48a3d6d0a1b06a0
Max Wofford <max@maxwofford.com>
2019-09-27 14:38:55 -0400

    Attempt to fix error throwing in /promo

--- src/interactions/promo.js
+++ src/interactions/promo.js
@@ -18,64 +18,74 @@ const promos = [
     details:
       'Available to club leaders. Must have a meeting time set with `/meeting-time`',
     run: (bot, message) =>
-      getInfoForUser(user).then(({ leader, club }) => {
-        if (!leader || !club) {
-          bot.replyPrivateDelayed(
-            message,
-            transcript('promos.githubGrant.notAuthorized')
-          )
-          return
-        }
-
-        return airFind('GitHub Grants', `{Club} = '${club.fields['ID']}'`).then(
-          grant => {
-            if (grant) {
-              if (club.fields['Leaders'].length == 1) {
-                bot.replyPrivateDelayed(
-                  message,
-                  transcript('promos.githubGrant.duplicate.soloLeader')
-                )
-              } else {
-                bot.replyPrivateDelayed(
-                  message,
-                  transcript('promos.githubGrant.duplicate.coleaders')
-                )
-              }
-              return
-            }
-            return airCreate('GitHub Grants', {
-              Club: [club.id],
-              Leader: [leader.id],
-              Type: 'First meeting ($100)',
-              'Club has HCB account': club.fields['HCB Account Requested'],
-              'Fee amount': 0,
-              'Grant amount': 100,
-            }).then(grant => {
-              const hcb = Boolean(club.fields['Club has HCB account'])
-              bot.replyPrivateDelayed(
-                message,
-                transcript('promos.githubGrant.success.hcbMessage.' + hcb, {
-                  firstLine: leader.fields['Address (first line)'],
-                  secondLine: leader.fields['Address (second line)'],
-                  city: leader.fields['Address (city)'],
-                  state: leader.fields['Address (state)'],
-                  zipCode: leader.fields['Address (zip code)'],
-                })
-              )
-              setTimeout(() => {
-                bot.replyPrivateDelayed(
-                  message,
-                  transcript(`promo.success.general`, {
-                    record: grant.id,
-                    hcb,
-                    user,
-                  })
-                )
-              }, 5000)
-            })
+      getInfoForUser(user)
+        .then(({ leader, club }) => {
+          if (!leader || !club) {
+            bot.replyPrivateDelayed(
+              message,
+              transcript('promos.githubGrant.notAuthorized')
+            )
+            return
           }
-        )
-      }),
+
+          return airFind('GitHub Grants', `{Club} = '${club.fields['ID']}'`)
+            .then(grant => {
+              if (grant) {
+                if (club.fields['Leaders'].length == 1) {
+                  bot.replyPrivateDelayed(
+                    message,
+                    transcript('promos.githubGrant.duplicate.soloLeader')
+                  )
+                } else {
+                  bot.replyPrivateDelayed(
+                    message,
+                    transcript('promos.githubGrant.duplicate.coleaders')
+                  )
+                }
+                return
+              }
+              return airCreate('GitHub Grants', {
+                Club: [club.id],
+                Leader: [leader.id],
+                Type: 'First meeting ($100)',
+                'Club has HCB account': club.fields['HCB Account Requested'],
+                'Fee amount': 0,
+                'Grant amount': 100,
+              })
+                .then(grant => {
+                  const hcb = Boolean(club.fields['Club has HCB account'])
+                  bot.replyPrivateDelayed(
+                    message,
+                    transcript('promos.githubGrant.success.hcbMessage.' + hcb, {
+                      firstLine: leader.fields['Address (first line)'],
+                      secondLine: leader.fields['Address (second line)'],
+                      city: leader.fields['Address (city)'],
+                      state: leader.fields['Address (state)'],
+                      zipCode: leader.fields['Address (zip code)'],
+                    })
+                  )
+                  setTimeout(() => {
+                    bot.replyPrivateDelayed(
+                      message,
+                      transcript(`promo.success.general`, {
+                        record: grant.id,
+                        hcb,
+                        user,
+                      })
+                    )
+                  }, 5000)
+                })
+                .catch(err => {
+                  throw err
+                })
+            })
+            .catch(err => {
+              throw err
+            })
+        })
+        .catch(err => {
+          throw err
+        }),
   },
 ]
 
