8d173e794986e3c9bce1d6363a3b88dd625b5c9e
Max Wofford <max@maxwofford.com>
2019-10-02 13:50:07 -0400

    attempt to fix errors not throwing in leaderInvite

--- src/interactions/leaderInvite.js
+++ src/interactions/leaderInvite.js
@@ -23,30 +23,38 @@ const invitePromise = leaderRecordID =>
 const interactionLeaderInvite = (bot, message) => {
   const { channel, user } = message
 
-  getInfoForUser(user).then(({ slackUser }) => {
-    if (!slackUser.is_owner) {
-      bot.reply(message, transcript('leaderInvite.notAdmin'))
-      return
-    }
-
-    // get all leaders for this channel
-    airFind('Clubs', 'Slack Channel ID', channel).then(club => {
-      if (!club) {
-        bot.reply(message, transcript('leaderInvite.notClubChannel'))
+  getInfoForUser(user)
+    .then(({ slackUser }) => {
+      if (!slackUser.is_owner) {
+        bot.reply(message, transcript('leaderInvite.notAdmin'))
+        return
       }
 
-      const invites = club.fields['Leaders'].map(invitePromise)
-      Promise.all(invites)
-        .then(values => {
-          console.log(values)
-          bot.reply(message, transcript('leaderInvite.success'))
+      // get all leaders for this channel
+      return airFind('Clubs', 'Slack Channel ID', channel)
+        .then(club => {
+          if (!club) {
+            bot.reply(message, transcript('leaderInvite.notClubChannel'))
+          }
+
+          const invites = club.fields['Leaders'].map(invitePromise)
+          return Promise.all(invites)
+            .then(values => {
+              console.log(values)
+              bot.reply(message, transcript('leaderInvite.success'))
+            })
+            .catch(err => {
+              throw err
+            })
         })
         .catch(err => {
-          console.error(err)
-          bot.reply(message, transcript('errors.general', { err }))
+          throw err
         })
     })
-  })
+    .catch(err => {
+      console.error(err)
+      bot.reply(message, transcript('errors.general', { err }))
+    })
 }
 
 export default interactionLeaderInvite
