c78f3f5d6678820efed9fbc85c4d63b515e99a96
Max Wofford <max@maxwofford.com>
2019-10-16 12:49:32 -0400

    Add initial attempt at sending messages

--- src/interactions/whisper.js
+++ src/interactions/whisper.js
@@ -1,12 +1,16 @@
 import { getInfoForUser } from '../utils'
 
 const interactionWhisper = (bot, message) => {
-  const { user } = message
+  const { user, text } = message
   getInfoForUser(user).then(({ slackUser }) => {
     if (!slackUser.is_owner) {
       throw new Error('This command is admin only')
     }
 
+    const messageRegex = /send a whisper to <@(.*?)>:(.*)/
+    const [, targetUser, targetMessage] = text.match(messageRegex)
+
+    bot.say({ text: targetMessage, channel: targetUser })
     console.log(message)
     console.log(message.text)
   })
