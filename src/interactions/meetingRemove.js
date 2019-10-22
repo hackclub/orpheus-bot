d51b485633a97e4c8a156e1cff77cb01745667fe
Max Wofford <max@maxwofford.com>
2019-10-22 14:07:40 -0400

    Catch errors in /meeting-remove

--- src/interactions/meetingRemove.js
+++ src/interactions/meetingRemove.js
@@ -2,27 +2,35 @@ import { getInfoForUser, airFind, transcript, airPatch } from '../utils'
 
 const interactionMeetingRemove = (bot, message) => {
   const { user, text } = message
-  getInfoForUser(user).then(({ leader, club, history }) => {
-    if (!leader) {
-      throw new Error('This command can only be run by club leaders')
-    }
+  getInfoForUser(user)
+    .then(({ leader, club, history }) => {
+      if (!leader) {
+        throw new Error('This command can only be run by club leaders')
+      }
 
-    if (!club) {
-      throw new Error('No club found')
-    }
+      if (!club) {
+        throw new Error('No club found')
+      }
 
-    const meeting = history.meetings.find(h => h.id === text)
+      const meeting = history.meetings.find(h => h.id === text)
 
-    if (meeting) {
-      airPatch('History', meeting.id, { 'Deleted At': Date.now() }).then(
-        record => {
-          bot.replyPrivateDelayed(message, transcript('meetingRemove.success'))
-        }
-      )
-    } else {
-      bot.replyPrivateDelayed(message, transcript('meetingRemove.help'))
-    }
-  })
+      if (meeting) {
+        airPatch('History', meeting.id, { 'Deleted At': Date.now() }).then(
+          record => {
+            bot.replyPrivateDelayed(
+              message,
+              transcript('meetingRemove.success')
+            )
+          }
+        )
+      } else {
+        bot.replyPrivateDelayed(message, transcript('meetingRemove.help'))
+      }
+    })
+    .catch(err => {
+      console.error(err)
+      bot.replyPrivateDelayed(message, transcript('errors.general', { err }))
+    })
 }
 
 export default interactionMeetingRemove
