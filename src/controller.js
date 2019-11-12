68a820f74619dc7439583c4600d487c83bea08ed
Max Wofford <max@maxwofford.com>
2019-11-12 17:59:27 -0500

    Comment scrying out while testing

--- src/controller.js
+++ src/controller.js
@@ -19,12 +19,12 @@ controller.middleware.heard.use((bot, message, convo, next) => {
     2 // https://stackoverflow.com/a/7220510
   )
 
-  const scryBot = controller.spawn({ token: process.env.SLACK_BOT_TOKEN, })
   console.log('scrying', stringifiedData)
-  scryBot.say({
-    text: transcript('mirror', { stringifiedData }),
-    channel: SCRYING_CHANNEL
-  })
+  // const scryBot = controller.spawn({ token: process.env.SLACK_BOT_TOKEN, })
+  // scryBot.say({
+  //   text: transcript('mirror', { stringifiedData }),
+  //   channel: SCRYING_CHANNEL
+  // })
   next()
 })
 
