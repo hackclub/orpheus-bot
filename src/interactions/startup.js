0eb52a19908a36ff7432a922762ceddd09aaba44
Max Wofford <max@maxwofford.com>
2019-11-12 17:41:48 -0500

    Clarify startup messages

--- src/interactions/startup.js
+++ src/interactions/startup.js
@@ -4,8 +4,8 @@ const BOT_SPAM_CHANNEL = 'C0P5NE354'
 
 const interactionStartup = (bot = initBot(), message) => {
   bot.say({
-    text: transcript('startup'),
-    channel: BOT_SPAM_CHANNEL
+    text: transcript('startup.message'),
+    channel: BOT_SPAM_CHANNEL,
   })
 }
 
