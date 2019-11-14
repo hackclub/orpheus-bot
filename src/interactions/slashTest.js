5f3c0709c3f5f1cf6df0946773b1ad1d39c6eb7f
Max Wofford <max@maxwofford.com>
2019-11-14 13:02:27 -0500

    Catch errors in test slash command

--- src/interactions/slashTest.js
+++ src/interactions/slashTest.js
@@ -1,12 +1,16 @@
 export default (bot, message) => {
-  bot.replyPrivateDelayed(message, 'initial message', (err, newMessage) => {
-    console.log(newMessage)
-    setTimeout(() => {
-      bot.api.chat.update({
-        channel: newMessage.channel,
-        text: 'new text',
-        ts: newMessage.ts
-      })
-    }, 2000)
-  })
+  try {
+    bot.replyPrivateDelayed(message, 'initial message', (err, newMessage) => {
+      console.log(newMessage)
+      setTimeout(() => {
+        bot.api.chat.update({
+          channel: newMessage.channel,
+          text: 'new text',
+          ts: newMessage.ts
+        })
+      }, 2000)
+    })
+  } catch (err) {
+    console.error(err)
+  }
 }
