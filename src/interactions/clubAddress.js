2d5b809b1fbfe5363df71b743eb06b26e0c0ce6c
Max Wofford <max@maxwofford.com>
2019-12-05 15:13:08 -0500

    Add clubAddress command finds or inits addresses

--- src/interactions/clubAddress.js
+++ src/interactions/clubAddress.js
@@ -1,21 +1,32 @@
 import { getInfoForUser, transcript } from '../utils'
 
-const interactionClubAddress = (bot, message) => {
-  const { user } = message
+const initAddress = async clubID => {
+  console.log(
+    `I couldn't find an address for ${clubID}, so I'm initializing a blank record`
+  )
+  const fields = {
+    Club: clubID,
+    'Currently assigned to Club': clubID,
+  }
+  await airCreate('Address', fields)
+}
 
-  getInfoForUser(user).then(({ leader, club, clubAddress }) => {
-    if (!leader) {
-      throw new Error('Command can only be run by leaders!')
-    }
-    if (!club) {
-      throw new Error('Club not found!')
-    }
+const interactionClubAddress = async (bot, message) => {
+  const { leader, club, clubAddress } = await getInfoForUser(message.user)
 
-    bot.replyPrivateDelayed(
-      message,
-      transcript('clubAddress', { address: clubAddress.fields })
-    )
-  })
+  if (!leader) {
+    throw new Error('Command can only be run by leaders!')
+  }
+  if (!club) {
+    throw new Error('Club not found!')
+  }
+
+  const address = clubAddress || (await initAddress(leader.id))
+
+  bot.replyPrivateDelayed(
+    message,
+    transcript('clubAddress', { address: address.fields })
+  )
 }
 
 export default interactionClubAddress
