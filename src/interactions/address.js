30e08ee63eed1d3231a5c02030490af725a8e1cd
Max Wofford <max@maxwofford.com>
2019-12-05 14:42:37 -0500

    Address command inits addresses if none are found

--- src/interactions/address.js
+++ src/interactions/address.js
@@ -1,19 +1,26 @@
-import { getInfoForUser, transcript } from '../utils'
+import { getInfoForUser, transcript, airCreate } from '../utils'
 
-const interactionAddress = (bot, message) => {
-  // check that they're a user
-  const { user } = message
-
-  getInfoForUser(user).then(({ person, personAddress }) => {
-    if (!person) {
-      throw new Error(`Couldn't find Slack ID in Airtable!`)
+const initAddress = async personID => {
+  const fields = {
+      Person: personID,
+      'Currently assigned to Person': personID
     }
+  await airCreate('Address', fields)
+}
 
-    bot.replyPrivateDelayed(
-      message,
-      transcript('address', { address: personAddress.fields })
-    )
-  })
+const interactionAddress = async (bot, message) => {
+  const { person, personAddress } = await getInfoForUser(message.user)
+
+  if (!person) {
+    throw new Error(`Couldn't find Slack ID in Airtable!`)
+  }
+
+  const address = personAddress || await initAddress(person.id)
+
+  bot.replyPrivateDelayed(
+    message,
+    transcript('address', { address })
+  )
 }
 
 export default interactionAddress
