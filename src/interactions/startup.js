d0f9a1c7a2f109033013d18d1f9ff16bc7f6474b
Max Wofford <max@maxwofford.com>
2019-12-26 23:20:25 -0500

    Only print start message in production

--- src/interactions/startup.js
+++ src/interactions/startup.js
@@ -3,6 +3,9 @@ import { initBot, transcript } from '../utils'
 const BOT_SPAM_CHANNEL = 'C0P5NE354'
 
 const interactionStartup = (bot = initBot(), message) => {
+  if (process.env.NODE_ENV === 'development') {
+    return
+  }
   bot.say({
     text: transcript('startup.message'),
     channel: BOT_SPAM_CHANNEL,
