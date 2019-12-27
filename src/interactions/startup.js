2474769714dab7f33d9fffe4b84a3e2bea2bad0c
Max Wofford <max@maxwofford.com>
2019-12-26 23:21:42 -0500

    Startup message logs to console instead in dev

--- src/interactions/startup.js
+++ src/interactions/startup.js
@@ -3,13 +3,12 @@ import { initBot, transcript } from '../utils'
 const BOT_SPAM_CHANNEL = 'C0P5NE354'
 
 const interactionStartup = (bot = initBot(), message) => {
+  const text = transcript('startup.message')
   if (process.env.NODE_ENV === 'development') {
-    return
+    console.log(text)
+  } else {
+    bot.say({ text, channel: BOT_SPAM_CHANNEL })
   }
-  bot.say({
-    text: transcript('startup.message'),
-    channel: BOT_SPAM_CHANNEL,
-  })
 }
 
 export default interactionStartup
