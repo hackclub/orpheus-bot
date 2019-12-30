af5ce58cd9c1b5f162416a2911648d465107b3c7
Max Wofford <max@maxwofford.com>
2019-12-30 12:27:07 -0500

    Add follow up steps to checkin reply

--- src/interactions/checkinReply.js
+++ src/interactions/checkinReply.js
@@ -1,17 +1,22 @@
-import { airCreate, getInfoForUser, transcript } from "../utils"
+import { airCreate, getInfoForUser, transcript } from '../utils'
 
 export default async (bot, message) => {
   const { channel, thread_ts, user, text, parent_user_id } = message
-  if (!thread_ts) { return }
+  if (!thread_ts) {
+    return
+  }
 
   try {
     const date = new Date(thread_ts * 1000).toISOString().replace(/T.*/, '')
-    const attendance = parseInt(text.replace( /[^\d.]/g, '' ))
+    const attendance = parseInt(text.replace(/[^\d.]/g, ''))
 
     const { club } = await getInfoForUser(user)
 
     if (!club || club.fields['Slack Channel ID'] != channel) {
-      bot.reply(message, transcript('checkinReply.notAuth', { attendance, channel }))
+      bot.reply(
+        message,
+        transcript('checkinReply.notAuth', { attendance, channel })
+      )
       return
     }
 
@@ -20,12 +25,19 @@ export default async (bot, message) => {
       Club: [club.id],
       Date: date,
       Attendance: attendance,
-      Notes: `@orpheus-bot created this entry from a Slack checkin`
+      Notes: '@orpheus-bot created this entry from a Slack checkin',
     }
 
     const meeting = await airCreate('History', fields)
 
-    bot.reply(message, transcript('checkinReply.success', { date: meeting.fields['Date'], attendance: meeting.fields['Attendance'] }))
+    bot.reply(
+      message,
+      transcript('checkinReply.success', {
+        date: meeting.fields['Date'],
+        attendance: meeting.fields['Attendance'],
+        photoURL: meeting.fields['Photo Upload URL'],
+      })
+    )
   } catch (err) {
     console.error(err)
     bot.reply(message, transcript('errors.general', { err }))
