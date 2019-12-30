068094dcd272a0ee9972fff088718650c33e202a
Max Wofford <max@maxwofford.com>
2019-12-30 09:13:11 -0500

    Write checkin notifications with async/await

--- src/interactions/checkinNotification.js
+++ src/interactions/checkinNotification.js
@@ -1,95 +1,34 @@
-import { initBot, airFind, userRecord, transcript } from '../utils'
+import { initBot, airFind, transcript } from '../utils'
 
-const interactionCheckinNotification = (bot = initBot(), message) => {
+const interactionCheckinNotification = async (bot = initBot(), message) => {
   const { channel } = message
   let { user } = message
 
+  // if there isn't a user provided we'll try and set one by looking up the club's POC
   if (!user) {
     console.log(
       transcript('checkinNotification.log.lookingForPOC', { channel })
     )
-    airFind('Clubs', 'Slack Channel ID', channel)
-      .then(club => {
-        const pocAirtableID = club.fields.POC
-        if (pocAirtableID) {
-          airFind('People', `RECORD_ID() = '${pocAirtableID}'`)
-            .then(poc => {
-              user = poc.fields['Slack ID']
-              console.log(
-                transcript('checkinNotification.log.foundPoc', {
-                  channel,
-                  user,
-                })
-              )
-              bot.say({
-                text: transcript('checkinNotification.named', { user }),
-                channel,
-              })
-            })
-            .catch(err => {
-              throw err
-            })
-        } else {
-          console.log(
-            transcript('checkinNotification.log.noPOCFound', { channel })
-          )
-          bot.say({ text: transcript('checkinNotification.unnamed'), channel })
-        }
-      })
-      .catch(err => {
-        throw err
-      })
-  } else {
+    const club = await airFind('Clubs', 'Slack Channel ID', channel)
+    const pocAirtableID = club.fields['POC']
+    let poc
+    if (pocAirtableID) {
+      poc = await airFind('People', `RECORD_ID() = '${pocAirtableID}'`)
+      user = poc.fields['Slack ID']
+    }
+  }
+
+  if (user) {
     console.log(
-      transcript('checkinNotification.log.posting', { user, channel })
+      transcript('checkinNotification.log.foundPoc', { channel, user })
     )
     bot.say({
       text: transcript('checkinNotification.named', { user }),
       channel,
     })
-
-    return // temporarily disabling this while we're using `/meeting-add` instead of checkin conversation
-    userRecord(user).then(userRecord => {
-      // If tutorial hasn't been run
-      if (
-        userRecord.fields['Flag: Tutorial /meeting-time'] &&
-        !userRecord.fields['Flag: reacted to checkin notification']
-      ) {
-        bot.whisper(
-          message,
-          {
-            blocks: [
-              {
-                type: 'section',
-                text: {
-                  type: 'mrkdwn',
-                  text:
-                    "We'll roleplay as if you've had a meeting, so go ahead and react",
-                },
-              },
-              {
-                type: 'image',
-                title: {
-                  type: 'plain_text',
-                  text: 'How to react to a message',
-                  emoji: true,
-                },
-                image_url: 'https://i.imgur.com/HVipvYp.gif',
-                alt_text: 'reacting to a message',
-              },
-            ],
-          },
-          (err, res) => {
-            if (err) {
-              console.error(err)
-            }
-            userRecord.patch({
-              'Flag: reacted to checkin notification': true,
-            })
-          }
-        )
-      }
-    })
+  } else {
+    console.log(transcript('checkinNotification.log.noPOCFound', { channel }))
+    bot.say({ text: transcript('checkinNotification.unnamed'), channel })
   }
 }
 
