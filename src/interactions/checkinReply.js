6961db85ffc14635df6799d4d88ea61253a3cf3a
Max Wofford <max@maxwofford.com>
2019-12-30 12:15:00 -0500

    Add error handling to checkin reply

--- src/interactions/checkinReply.js
+++ src/interactions/checkinReply.js
@@ -4,25 +4,30 @@ export default async (bot, message) => {
   const { channel, thread_ts, user, text, parent_user_id } = message
   if (!thread_ts) { return }
 
-  const date = new Date(thread_ts * 1000).toISOString().replace(/T.*/, '')
-  const attendance = parseInt(text.replace( /[^\d.]/g, '' ))
+  try {
+    const date = new Date(thread_ts * 1000).toISOString().replace(/T.*/, '')
+    const attendance = parseInt(text.replace( /[^\d.]/g, '' ))
 
-  const { club } = await getInfoForUser(user)
+    const { club } = await getInfoForUser(user)
 
-  if (!club || club.fields['Slack Channel ID'] != channel) {
-    bot.reply(message, transcript('checkinReply.notAuth', { attendance, channel }))
-    return
+    if (!club || club.fields['Slack Channel ID'] != channel) {
+      bot.reply(message, transcript('checkinReply.notAuth', { attendance, channel }))
+      return
+    }
+
+    const fields = {
+      Type: ['Meeting'],
+      Club: [club.id],
+      Date: date,
+      Attendance: attendance,
+      Notes: `@orpheus-bot created this entry from a Slack checkin`
+    }
+
+    const meeting = await airCreate('History', fields)
+
+    bot.reply(message, transcript('checkinReply.success', { date: meeting.fields['Date'], attendance: meeting.fields['Attendance'] }))
+  } catch (err) {
+    console.error(err)
+    bot.reply(message, transcript('errors.general', { err }))
   }
-
-  const fields = {
-    Type: ['Meeting'],
-    Club: [club.id],
-    Date: date,
-    Attendance: attendance,
-    Notes: `@orpheus-bot created this entry from a Slack checkin`
-  }
-
-  const meeting = await airCreate('History', fields)
-
-  bot.reply(message, transcript('checkinReply.success', { date: meeting.fields['Date'], attendance: meeting.fields['Attendance'] }))
 }
