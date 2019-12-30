49844184e1efa29c39b4d7d75aae0e1a2ed94e44
Max Wofford <max@maxwofford.com>
2019-12-30 12:11:38 -0500

    General improvements to checkin replys

--- src/interactions/checkinReply.js
+++ src/interactions/checkinReply.js
@@ -1,32 +1,28 @@
-import { airCreate, getInfoForUser } from "../utils"
+import { airCreate, getInfoForUser, transcript } from "../utils"
 
 export default async (bot, message) => {
   const { channel, thread_ts, user, text, parent_user_id } = message
   if (!thread_ts) { return }
 
+  const date = new Date(thread_ts * 1000).toISOString().replace(/T.*/, '')
+  const attendance = parseInt(text.replace( /[^\d.]/g, '' ))
+
   const { club } = await getInfoForUser(user)
 
-  if (!club) {
-    bot.reply(`I thought you were asking me to record a meeting but I'm not because you're not a registered leader`)
+  if (!club || club.fields['Slack Channel ID'] != channel) {
+    bot.reply(message, transcript('checkinReply.notAuth', { attendance, channel }))
     return
   }
 
-  if (club.fields['Slack Channel ID'] != channel) {
-    bot.reply(message, `I thought you were telling me to record a meeting, but I'm not going to because this isn't your club channel`)
-    return
-  }
-
-  const date = new Date(thread_ts * 1000).toISOString().replace(/T.*/, '')
-  console.log('parsed date', date)
   const fields = {
     Type: ['Meeting'],
     Club: [club.id],
     Date: date,
-    Attendance: parseInt(text.replace( /[^\d.]/g, '' )),
+    Attendance: attenance,
     Notes: `@orpheus-bot created this entry from a Slack checkin`
   }
 
   const meeting = await airCreate('History', fields)
 
-  bot.reply(message, `I've recorded a club meeting <!date^${new Date(meeting.fields['Date']).getTime()}^{date_short_pretty}|on ${meeting.fields['Date']}> with ${meeting.fields['Attendance']} members`)
+  bot.reply(message, transcript('checkinReply.success', { date: meeting.fields['Date'], attendance: meeting.fields['Attendance'] }))
 }
