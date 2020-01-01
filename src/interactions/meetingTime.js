3bbd84d852ab1d671225a638c2bc13fa00d3be52
Max Wofford <max@maxwofford.com>
2020-01-01 10:41:38 -0500

    Improve /meeting-time success message

--- src/interactions/meetingTime.js
+++ src/interactions/meetingTime.js
@@ -46,7 +46,7 @@ const interactionMeetingTime = (bot, message) => {
         .then(record => {
           bot.replyPrivateDelayed(
             message,
-            transcript('meetingTime.success', { offsetDate }),
+            transcript('meetingTime.success', { offsetDate, channelID: club.fields['Slack Channel ID'] }),
             (err, res) => {
               if (err) {
                 throw err
