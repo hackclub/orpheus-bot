ff4a618b04fae16b997743e272a27bd369708128
Max Wofford <max@maxwofford.com>
2020-06-18 02:33:38 -0400

    Record approvers using som-promote

--- src/interactions/som/promote.js
+++ src/interactions/som/promote.js
@@ -1,16 +1,22 @@
 import FormData from 'form-data'
-import { getInfoForUser, transcript, initBot } from "../../utils"
+import { getInfoForUser, transcript, initBot, airPatch, airFind } from '../../utils'
 import fetch from 'isomorphic-unfetch'
 
-const approveUser = async (user) => new Promise((resolve, reject) => {
-  const form = new FormData()
-  form.append('user', user)
-  form.append('token', process.env.SLACK_INVITE_TOKEN)
-  fetch('https://slack.com/api/users.admin.setRegular?slack_route=T0266FRGM', {
-    method: 'POST',
-    body: form
-  }).then(res => resolve(res)).catch(err => reject(err))
-})
+const approveUser = async (user) =>
+  new Promise((resolve, reject) => {
+    const form = new FormData()
+    form.append('user', user)
+    form.append('token', process.env.SLACK_INVITE_TOKEN)
+    fetch(
+      'https://slack.com/api/users.admin.setRegular?slack_route=T0266FRGM',
+      {
+        method: 'POST',
+        body: form,
+      }
+    )
+      .then((res) => resolve(res))
+      .catch((err) => reject(err))
+  })
 
 const interactionSOMPromote = async (bot = initBot(), message) => {
   const taggedUserID = (message.text.match(/<@([a-zA-Z0-9]*)|/) || [])[1]
@@ -21,21 +27,31 @@ const interactionSOMPromote = async (bot = initBot(), message) => {
   user.tagged = await getInfoForUser(taggedUserID)
   user.caller = await getInfoForUser(message.user)
   if (user.caller.slackUser.is_restricted) {
-    console.log("caller is restricted, cancelling")
+    console.log('caller is restricted, cancelling')
     return
   }
   if (!user.tagged.slackUser.is_restricted) {
-    console.log("tagged is not restricted, cancelling")
+    console.log('tagged is not restricted, cancelling')
+    return
+  }
+
+  const guest = await airFind(
+    'Join Requests',
+    `AND(NOT(Approver=BLANK()),{Email Address}='${user.tagged.person.fields['Email']}')`,
+    null,
+    { base: 'som' }
+  )
+
+  if (!guest) {
+    console.log('tagged is not an SOM guest, cancelling')
     return
   }
 
   try {
-    const result = await approveUser(taggedUserID)
+    await airPatch('Join Requests', guest.id, { Approver: message.user }, { base: 'som' })
+    await approveUser(taggedUserID)
 
-    bot.replyPrivateDelayed(
-      message,
-      transcript('som.approve.success')
-    )
+    bot.replyPrivateDelayed(message, transcript('som.approve.success'))
   } catch (e) {
     console.error(e)
   }
