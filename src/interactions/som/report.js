98dce21a78e39306ddc314727721e4443a23ad36
Max Wofford <max@maxwofford.com>
2020-06-18 02:31:38 -0400

    Use transcript for som-report

--- src/interactions/som/report.js
+++ src/interactions/som/report.js
@@ -1,22 +1,20 @@
-import { getInfoForUser, airCreate } from '../../utils'
+import { airCreate, transcript } from '../../utils'
 
 const interactionSOMReport = async (bot, message) => {
   const taggedUserID = (message.text.match(/<@([a-zA-Z0-9]*)|/) || [])[1]
   if (!taggedUserID) {
-    console.log("No tagged user!")
+    bot.replyPrivateDelayed(message, transcript('som.report.help'))
     return // do something if we don't tag a user
   }
-
-  // const submitter = await getInfoForUser(message.user)
-  // const subject = await getInfoForUser(taggedUserID)
 
   const report = await airCreate('Conduct Reports', {
     'Submitter Slack ID': message.user,
     'Subject Slack ID': taggedUserID,
   }, { base: 'som' })
+
   const reportFormUrl = 'http://hack.af/slack-report?prefill_Report%20ID=' + report.id
 
-  bot.replyPrivateDelayed(message, reportFormUrl)
+  bot.replyPrivateDelayed(message, transcript('som.report.submitted', { reportFormUrl, subject: taggedUserID }))
 }
 
 export default interactionSOMReport
