520eff9a296891ce84285118622fb1933fe3d02e
Matthew Stanciu <mattbstanciu@gmail.com>
2020-06-23 14:56:12 -0400

    Add debug logs

--- src/interactions/som/ban.js
+++ src/interactions/som/ban.js
@@ -4,13 +4,16 @@ const { initBot, transcript } = require("../../utils");
 const interactionSOMBan = async (bot = initBot(), message) => {
   const taggedUserID = (message.text.match(/<@([a-zA-Z0-9]*)|/) || [])[1]
   if (!taggedUserID) {
+    console.log('ban: no tagged user')
     return // do something if we don't tag a user
   }
   const admin = await isAdmin(bot, message.user)
   if (!admin) {
+    console.log('not admin!')
     bot.replyPrivate(message, transcript('som.ban.notAdmin'))
   }
   else {
+    console.log('deactivating ', taggedUserID)
     await Promise.all([
       fetch(`https://slack.com/api/users.admin.setInactive?token=${process.env.SLACK_LEGACY_TOKEN}&user=${taggedUserID}`).then(r => r.json()),
       bot.replyPrivateDelayed(message, transcript('som.ban.deactivated', { user: taggedUserID }))
