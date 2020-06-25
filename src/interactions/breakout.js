89355b9f2b132e058da81dd7aa3947922b05ea3a
Max Wofford <max@maxwofford.com>
2020-06-25 14:12:59 -0400

    Attempt to generate new breakout channels

--- src/interactions/breakout.js
+++ src/interactions/breakout.js
@@ -1,14 +1,40 @@
 import { getInfoForUser, initBot, airCreate, transcript } from '../utils'
 
-const getChannelName = async (channel) => {
-  initBot().api.conversations.info({
-    channel
+const getChannelName = async (channel) =>
+  new Promise((resolve, reject) => {
+    initBot().api.conversations.info(
+      {
+        channel,
+      },
+      (err, res) => {
+        if (err) {
+          console.error(err)
+          reject(err)
+        }
+        console.log('getChannelName', res)
+        resolve(res.channel.name)
+      }
+    )
   })
-}
 
 const createUniqueChannel = async (channel) => {
   const baseName = await getChannelName(channel)
-  return baseName + '-' + Math.random().toString().substr(2, 5)
+  console.log('baseName', baseName)
+  const name = baseName + '-' + Math.random().toString().substr(2, 5)
+
+  return new Promise((resolve, reject) => {
+    initBot().api.conversations.create(
+      {
+        name,
+      },
+      (err, res) => {
+        if (err) {
+          reject(err)
+        }
+        resolve(res)
+      }
+    )
+  })
 }
 
 const interactionBreakout = async (bot, message) => {
@@ -24,11 +50,14 @@ const interactionBreakout = async (bot, message) => {
   await airCreate('Breakout Channel', {
     'Breakout Channel ID': breakoutChannel,
     'Parent Channel ID': channel,
-    'Creator': user,
-    'Creation Timestamp': timestamp
+    Creator: user,
+    'Creation Timestamp': timestamp,
   })
 
-  bot.reply(message, transcript('breakout.created', { channel: breakoutChannel }))
+  bot.reply(
+    message,
+    transcript('breakout.created', { channel: breakoutChannel })
+  )
 }
 
 export default interactionBreakout
