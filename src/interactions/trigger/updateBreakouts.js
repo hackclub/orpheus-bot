9387d14d1ec23a8f7c702fda5ae10f00e1f7db97
Max Wofford <max@maxwofford.com>
2020-06-26 10:15:43 -0400

    Refactor breakout updates to include a warning step

--- src/interactions/trigger/updateBreakouts.js
+++ src/interactions/trigger/updateBreakouts.js
@@ -6,74 +6,41 @@ export default async (bot = initBot(), message) => {
     '{Archived Channel Name} = BLANK()'
   )
   breakoutChannels.map(async breakout => {
-    bot.replyInThread(message, `<#${breakout.fields['Breakout Channel ID']}>`)
     bot.api.conversations.history(
       { channel: breakout.fields['Breakout Channel ID'] },
       (err, res) => {
         if (err) {
-          console.error('stage 1', err)
-          /* do something */
+          console.error(err)
         }
         const { messages } = res
-        console.log(
-          'Here are the timestamps lord timekeeper!',
-          messages.map(m => m.ts)
-        )
+
         const latestTimestamp = messages.map(m => m.ts).sort()[
           messages.length - 1
         ]
+
         const timeSinceLastUpdate =
-          Date.now() - parseInt(latestTimestamp.replace('.',''))/1000
-        console.log('time since last update in channel', breakout.id, 'was', timeSinceLastUpdate)
-        if (timeSinceLastUpdate > 1000 * 60 * 1) {
+          Date.now() - parseInt(latestTimestamp.replace('.', '')) / 1000
+        const overTimeLimit = timeSinceLastUpdate > 1000 * 60 * 30
+
+        // we should close the channel if the last post was @orpheus' warning that archiving is coming
+        if (
+          overTimeLimit &&
+          latestTimestamp == breakout.fields['Archive Warning Timestamp']
+        ) {
           bot.replyInThread(
             message,
-            `Closing <#${breakout.fields['Breakout Channel ID']}> because it's been ${timeSinceLastUpdate / 1000} seconds since last post`
+            `Closing <#${breakout.fields['Breakout Channel ID']}> because the most recent post was me saying`
           )
-          const archivedName =
-            'archived-' +
-            breakout.fields['Breakout Channel Name'] +
-            '-' +
-            new Date()
-              .toISOString()
-              .slice(0, 19)
-              .replace(/:/g, '-')
-              .replace('T', '_')
-          bot.api.conversations.rename(
-            {
-              channel: breakout.fields['Breakout Channel ID'],
-              name: archivedName,
-            },
-            (err, res) => {
-              if (err) {
-                console.error('stage 2', err)
-                /* do something */
-              }
-              // we need an admin token for this b/c Hack Club's Slack settings make archiving return 'restricted_action'
-              initBot(true).api.conversations.archive(
-                { channel: breakout.fields['Breakout Channel ID'] },
-                (err, res) => {
-                  if (err) {
-                    console.error('stage 3', err)
-                    /* do something */
-                  }
-                  airPatch('Breakout Channel', breakout.id, {
-                    'Archived Channel Name': archivedName,
-                  })
-                  console.log(
-                    'I just archived',
-                    breakout.fields['Breakout Channel ID']
-                  )
-                }
-              )
-            }
-          )
-        } else {
-          console.log('time to update the latest ts!')
+          closeBreakout(bot, message, breakout)
+        } else if (overTimeLimit) {
+          // we should warn the channel it will be archived if there is no activity within 30 minutes
           bot.replyInThread(
             message,
             `Keeping open <#${breakout.fields['Breakout Channel ID']}>`
           )
+          warnBreakout(bot, message, breakout)
+        } else {
+          // not over the time limit? That's fine– let's update the timestamp
           airPatch('Breakout Channel', breakout.id, {
             'Last Updated Timestamp': latestTimestamp,
           })
@@ -83,6 +50,50 @@ export default async (bot = initBot(), message) => {
   })
 }
 
-// When online...
-// Add " Last Updated Timestamp" column to "Breakout Channel" table
-// Add "Open" checkbox to "breakout channel table"
+const closeBreakout = async (bot, message, breakout) => {
+  const archivedName =
+    'archived-' +
+    breakout.fields['Breakout Channel Name'] +
+    '-' +
+    new Date().toISOString().slice(0, 19).replace(/:/g, '-').replace('T', '_')
+  bot.api.conversations.rename(
+    {
+      channel: breakout.fields['Breakout Channel ID'],
+      name: archivedName,
+    },
+    (err, res) => {
+      if (err) {
+        console.error(err)
+      }
+      // we need an admin token for this b/c Hack Club's Slack settings make archiving return 'restricted_action'
+      initBot(true).api.conversations.archive(
+        { channel: breakout.fields['Breakout Channel ID'] },
+        (err, res) => {
+          if (err) {
+            console.error('stage 3', err)
+          }
+          airPatch('Breakout Channel', breakout.id, {
+            'Archived Channel Name': archivedName,
+          })
+          console.log('I just archived', breakout.fields['Breakout Channel ID'])
+        }
+      )
+    }
+  )
+}
+
+const warnBreakout = async (bot, message, breakout) => {
+  bot.say(
+    { text: transcript('breakout'), channel: breakout.id },
+    (err, res) => {
+      if (err) {
+        console.error(err)
+      }
+
+      airPatch('Breakout Channel', breakout.id, {
+        'Last Updated Timestamp': res.ts,
+        'Archive Warning Timestamp': res.ts,
+      })
+    }
+  )
+}
