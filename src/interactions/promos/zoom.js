bcdea706291df7a59495fdda2022511acab9d63d
Max Wofford <max@maxwofford.com>
2020-09-05 01:55:47 -0400

    Run fmt

--- src/interactions/promos/zoom.js
+++ src/interactions/promos/zoom.js
@@ -2,11 +2,9 @@ import fetch from 'isomorphic-unfetch'
 
 import { getInfoForUser, airFind, airPatch, transcript } from '../../utils'
 
-export const names = [
-  'Zoom Pro',
-  'zoom',
-]
-export const details = 'Available for one club leader at each club (the point of contact). They can add their co-leaders and members to their account.'
+export const names = ['Zoom Pro', 'zoom']
+export const details =
+  'Available for one club leader at each club (the point of contact). They can add their co-leaders and members to their account.'
 export async function run(bot, message) {
   const { user } = message
   const { leader, club } = await getInfoForUser(user)
@@ -46,17 +44,24 @@ export async function run(bot, message) {
   if (!leader.fields['Zoom ID']) {
     // no Zoom ID set in Airtable? We'll update their airRecord with the newly
     // created Zoom account ID in the background
-    airPatch('People', leader.id, {'Zoom ID': zoomID})
+    airPatch('People', leader.id, { 'Zoom ID': zoomID })
     bot.replyPrivateDelayed(
       message,
-      transcript('promos.zoom.createdAccount', { email: leader.fields['Email'] })
+      transcript('promos.zoom.createdAccount', {
+        email: leader.fields['Email'],
+      })
     )
   } else {
     // they already have a Zoom account– let's toggle the account type
     const zoomAccount = getZoomUser(zoomID)
     if (!zoomAccount.type) {
       // account is 'pending' activation– ask the user to check their email
-      bot.replyPrivateDelayed(message, transcript('promos.zoom.pendingAccount', { email: leader.fields['Email'] }))
+      bot.replyPrivateDelayed(
+        message,
+        transcript('promos.zoom.pendingAccount', {
+          email: leader.fields['Email'],
+        })
+      )
     } else {
       // toggle account type
       bot.replyPrivateDelayed(message, transcript('promos.zoom.toggleAccount'))
@@ -68,7 +73,7 @@ export async function run(bot, message) {
 import jwt from 'jsonwebtoken'
 const payload = {
   iss: process.ZOOM_KEY,
-  exp: ((new Date()).getTime() + 5000),
+  exp: new Date().getTime() + 5000,
 }
 const token = jwt.sign(payload, process.env.ZOOM_SECRET)
 
@@ -76,7 +81,7 @@ async function getZoomUser(zoomID) {
   const result = await fetch(`https://api.zoom.us/v2/users/${zoomID}`, {
     method: 'GET',
     headers: {
-      'authorization': `Bearer ${token}`
+      authorization: `Bearer ${token}`,
     },
   })
 
@@ -90,20 +95,20 @@ async function createZoomUser(person) {
   const email = person.fields['Email']
 
   const body = JSON.stringify({
-    action: "create",
+    action: 'create',
     user_info: {
       type: 2, // Licensed
       first_name,
       last_name,
       email,
-    }
+    },
   })
 
   const result = await fetch('https://api.zoom.us/v2/users', {
     method: 'POST',
     headers: {
       'content-type': 'application/json',
-      'authorization': `Bearer ${token}`
+      authorization: `Bearer ${token}`,
     },
     body,
   })
@@ -113,14 +118,14 @@ async function createZoomUser(person) {
 
 async function updateZoomUser(zoomID, upOrDowngrade) {
   const body = JSON.stringify({
-    type: upOrDowngrade == 'upgrade' ? 2 : 1
+    type: upOrDowngrade == 'upgrade' ? 2 : 1,
   })
 
   const result = await fetch(`https://api.zoom.us/v2/users/${zoomID}`, {
     method: 'PATCH',
     headers: {
       'content-type': 'application/json',
-      'authorization': `Bearer ${token}`
+      authorization: `Bearer ${token}`,
     },
     body,
   })
