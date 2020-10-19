983f5cc7391c185600e8a2217f936dd922f2e2f2
Max Wofford <max@maxwofford.com>
2020-10-19 12:46:39 -0400

    Update hacktoberfest to 2020

--- src/interactions/hacktoberfest.js
+++ src/interactions/hacktoberfest.js
@@ -8,8 +8,8 @@ const interactionHacktoberfest = (bot, message) => {
 
   const { user, channel } = message
   return getInfoForUser(user).then(({ userRecord }) => {
-    if (!userRecord.fields['Flag: Hacktoberfest 2019']) {
-      userRecord.patch({ 'Flag: Hacktoberfest 2019': true })
+    if (!userRecord.fields['Flag: Hacktoberfest 2020']) {
+      userRecord.patch({ 'Flag: Hacktoberfest 2020': true })
 
       bot.sendEphemeral({ channel, user, text: transcript('hacktoberfest') })
     }
