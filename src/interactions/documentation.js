ac114323381f6f9a2cab24d5418fa1a0f9f4889b
Max Wofford <max@maxwofford.com>
2020-10-19 15:29:11 -0400

    Add experimental message updater

--- src/interactions/documentation.js
+++ src/interactions/documentation.js
@@ -4,19 +4,40 @@ const documentationURL = 'https://github.com/hackclub/orpheus-bot'
 
 const interactionDocumentation = async (bot, message) => {
   try {
-    let sent = await bot.reply(message, 'READ')
-    sent = await bot.updateMessage({
-      text: 'read MY',
-      ...sent
-    })
-    sent = await bot.updateMessage({
-      text: 'read my DOCS',
-      ...sent
-    })
-    sent = await bot.updateMessage({
-      text: 'read my docs',
-      ...sent
-    })
+
+    bot.replyAndUpdate(
+      message,
+      'READ',
+      (err, src, updateResponse) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        setTimeout(() => {
+          updateResponse('read MY', err => {
+            if (err) console.error(err)
+          })
+          setTimeout(() => {
+            updateResponse('read my DOCS', err => {
+              if (err) console.error(err)
+            })
+          }, Math.random() * 1000 + 1000)
+        }, Math.random() * 1000 + 1000)
+      }
+    )
+    // let sent = await bot.reply(message, 'READ')
+    // sent = await bot.updateMessage({
+    //   text: 'read MY',
+    //   ...sent
+    // })
+    // sent = await bot.updateMessage({
+    //   text: 'read my DOCS',
+    //   ...sent
+    // })
+    // sent = await bot.updateMessage({
+    //   text: 'read my docs',
+    //   ...sent
+    // })
   } catch (err) {
     console.error(err)
     bot.reply(message, transcript('errors.general', { err }))
