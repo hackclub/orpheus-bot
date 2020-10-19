609fd1b35b4b23622b19af6a5d322765e135971a
Max Wofford <max@maxwofford.com>
2020-10-19 16:11:13 -0400

    Try running entirely dynamic fancy text

--- src/interactions/documentation.js
+++ src/interactions/documentation.js
@@ -1,88 +1,48 @@
 import { transcript } from "../utils"
 
-const documentationURL = 'https://github.com/hackclub/orpheus-bot'
-
 const replyFancy = async ({bot, message, textSteps}) => {
-  bot.replyAndUpdate(message, (err, src, updateResponse) => {
-    if (err) {
-      console.error(err)
-      return
+  bot.replyAndUpdate(
+    message,
+    textSteps[0],
+    async (err, src, updateResponse) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      for (const step of textSteps) {
+        await new Promise((resolve,reject)=> {
+          updateResponse(step, err => {
+            if (err) reject(err)
+            resolve()
+          })
+        })
+      }
     }
-    textSteps.map(step => {
-    })
-  })
+  )
+}
+
+const randomFlavor = (l) => {
+  if (!/^[a-z0-9]+$/i.test(l)) {
+    return l
+  }
+  switch (Math.floor(Math.random() * 3)) {
+    case 0:
+      return `*${l.toUpperCase()}*`
+    case 1:
+      return `**${l.toUpperCase()}**`
+    case 2:
+      return `_${l.toUpperCase()}_`
+  }
 }
 
 const interactionDocumentation = async (bot, message) => {
   try {
-    const textSteps = [
-      'R',
-      'rE',
-      'reA',
-      'reaD',
-      'read ',
-      'read M',
-      'read mY',
-      'read my ',
-      'read my D',
-      'read my dO',
-      'read my doC',
-      'read my docS',
-      'read my docs',
-    ]
-    bot.replyAndUpdate(
-      message,
-      textSteps[0],
-      async (err, src, updateResponse) => {
-        if (err) {
-          console.error(err)
-          return
-        }
-        for (const step of textSteps) {
-          await new Promise((resolve,reject)=> {
-            updateResponse(step, err => {
-              if (err) reject(err)
-              resolve()
-            })
-          })
-        }
-      }
-    )
+    const textSteps = transcript('documentation.flavortext').split('').map((letter, i, array) => {
+      const text = array.slice(0, i).join('') + randomFlavor(array[i])
+      return transcript('documentation.formatLink', { text } )
+    })
 
-    // bot.replyAndUpdate(
-    //   message,
-    //   'READ',
-    //   (err, src, updateResponse) => {
-    //     if (err) {
-    //       console.error(err)
-    //       return
-    //     }
-    //     setTimeout(() => {
-    //       updateResponse('read MY', err => {
-    //         if (err) console.error(err)
-    //       })
-    //       setTimeout(() => {
-    //         updateResponse('read my DOCS', err => {
-    //           if (err) console.error(err)
-    //         })
-    //       }, Math.random() * 1000 + 1000)
-    //     }, Math.random() * 1000 + 1000)
-    //   }
-    // )
-
-    // let sent = await bot.reply(message, 'READ')
-    // sent = await bot.updateMessage({
-    //   text: 'read MY',
-    //   ...sent
-    // })
-    // sent = await bot.updateMessage({
-    //   text: 'read my DOCS',
-    //   ...sent
-    // })
-    // sent = await bot.updateMessage({
-    //   text: 'read my docs',
-    //   ...sent
-    // })
+    await replyFancy({bot, message, textSteps})
   } catch (err) {
     console.error(err)
     bot.reply(message, transcript('errors.general', { err }))
