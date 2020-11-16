efe4aa1726a20c69c5450f03805624a869e1aa40
Max Wofford <max@maxwofford.com>
2020-11-16 10:59:33 -0500

    Meeting remove shouldn't fail when meeting list isn't found

--- src/interactions/meetingRemove.js
+++ src/interactions/meetingRemove.js
@@ -1,36 +1,41 @@
-import { getInfoForUser, airFind, transcript, airPatch } from '../utils'
+import { getInfoForUser, transcript, airPatch } from '../utils'
 
-const interactionMeetingRemove = (bot, message) => {
+const interactionMeetingRemove = async (bot, message) => {
   const { user, text } = message
-  getInfoForUser(user)
-    .then(({ leader, club, history }) => {
-      if (!leader) {
-        throw new Error('This command can only be run by club leaders')
-      }
 
-      if (!club) {
-        throw new Error('No club found')
-      }
+  try {
+    const { leader, club, history } = await getInfoForUser(user)
 
-      const meeting = history.meetings.find(h => h.id === text)
+    if (text.trim == 'help' || text.trim == '') {
+      bot.replyPrivateDelayed(message, transcript('meetingRemove.help'))
+      return
+    }
 
-      if (meeting) {
-        airPatch('History', meeting.id, { 'Deleted At': Date.now() }).then(
-          record => {
-            bot.replyPrivateDelayed(
-              message,
-              transcript('meetingRemove.success')
-            )
-          }
-        )
-      } else {
-        bot.replyPrivateDelayed(message, transcript('meetingRemove.help'))
-      }
-    })
-    .catch(err => {
-      console.error(err)
-      bot.replyPrivateDelayed(message, transcript('errors.general', { err }))
-    })
+    if (!leader) {
+      throw new Error('This command can only be run by club leaders')
+    }
+
+    if (!club) {
+      throw new Error('No club found')
+    }
+
+    const meeting = history.meetings.find(h => h.id === text)
+    if (meeting) {
+      airPatch('History', meeting.id, { 'Deleted At': Date.now() }).then(
+        _record => {
+          bot.replyPrivateDelayed(
+            message,
+            transcript('meetingRemove.success')
+          )
+        }
+      )
+    } else {
+      bot.replyPrivateDelayed(message, transcript('meetingRemove.help'))
+    }
+  } catch (err) {
+    console.error(err)
+    bot.replyPrivateDelayed(message, transcript('errors.general', { err }))
+  }
 }
 
 export default interactionMeetingRemove
