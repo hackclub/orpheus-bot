6c201c9d018de83cdff5a0632c887140e55bcc8e
Max Wofford <max@maxwofford.com>
2021-01-18 14:57:17 -0500

    Update reportForm.js

--- src/interactions/reportForm.js
+++ src/interactions/reportForm.js
@@ -1,8 +1,8 @@
 import { transcript } from "../utils"
 
 const interactionReportForm = async (bot, message) => {
-  const { trigger_id, callback_id } = message
-  const quote = message.message.text
+  const { trigger_id } = message
+  const quote = 'message.message.text'
 
   await bot.api.views.open({trigger_id, view: transcript('reportForm', {quote})})
 
