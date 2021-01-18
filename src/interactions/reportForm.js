2b87e4f01705bfe90b7ef281fe545687adc0c75b
Max Wofford <max@maxwofford.com>
2021-01-18 16:40:15 -0500

    Create a flag report on submit

--- src/interactions/reportForm.js
+++ src/interactions/reportForm.js
@@ -1,7 +1,8 @@
 import { transcript } from "../utils"
 
 const interactionReportForm = async (bot, message) => {
-  const { trigger_id } = message
+  const { trigger_id, channel } = message
+  console.log(JSON.stringify(message, undefined, 2))
   const quote = 'message.message.text'
 
   await bot.api.views.open({trigger_id, view: transcript('reportForm', {quote})})
--- src/interactions/reportFormSubmit.js
+++ src/interactions/reportFormSubmit.js
@@ -1,5 +1,18 @@
-const interactionReportFormSubmit = async (bot, message) => {
-  console.log(message.view.state)
+import { airCreate } from "../utils"
 
+const interactionReportFormSubmit = async (bot, message) => {
+  const notes = message.view.state.values.notes.notesValue.value
+
+  const report = await airCreate(
+    'User Instance',
+    { 'Slack ID': message.user, },
+    { base: 'conduct' }
+  )
+
+  const submission = await airCreate('Submission', {
+    'Report ID': report.id,
+    'Summary': 'This would normally contain a link to the post',
+    'Notes': notes
+  })
 }
 export default interactionReportFormSubmit
