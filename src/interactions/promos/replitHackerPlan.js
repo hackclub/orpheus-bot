7fc878a10dcc0708b2b2d75d3a718d0b08eecb21
Matthew Stanciu <mattbstanciu@gmail.com>
2021-02-23 13:10:44 -0500

    Add replit

---
+++ src/interactions/promos/replitHackerPlan.js
@@ -0,0 +1,26 @@
+import { transcript } from '../../utils'
+
+export const names = [
+  'Replit',
+  'Repl.it',
+  'Repl.it Hacker Plan',
+  'Replit Hacker Plan',
+  'Repl.it Hacker',
+  'Replit Hacker'
+]
+
+export const details = 'Available to anyone'
+export async function run(bot, message) {
+  const { user } = message
+  const { leader, club } = await getInfoForUser(user)
+
+  if (!leader || !club) {
+    await bot.replyPrivateDelayed(
+      message,
+      transcript('promos.replit.notAuthorized')
+    )
+    return
+  }
+
+  await bot.replyPrivateDelayed(message, transcript('promos.replit.success'))
+}
