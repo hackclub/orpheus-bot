6c1077cbba94fe1376bf9cabe6c1cb1102b1f88d
Max Wofford <max@maxwofford.com>
2021-08-19 01:02:53 -0400

    Attempt thread support for wahoo fish

--- src/index.js
+++ src/index.js
@@ -106,7 +106,7 @@ controller.hears(
 
 controller.hears(
   ['wahoo', 'fish'],
-  'mention,indirect_mention,direct_mention,direct_message,ambient',
+  'mention,indirect_mention,direct_mention,direct_message,ambient,message_replied',
   (bot, message) => {
     bot.replyInThread(message, transcript("wahooFish"));
   }
