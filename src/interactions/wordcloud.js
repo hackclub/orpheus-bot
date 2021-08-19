fdda2472867c3f88ce8c1ebfb0bce2c693d72759
Max Wofford <max@maxwofford.com>
2021-08-18 23:37:10 -0400

    Update wordcloud.js

--- src/interactions/wordcloud.js
+++ src/interactions/wordcloud.js
@@ -43,22 +43,22 @@ const interactionWordcloud = async (bot = initBot(true), message) => {
 
   // let updatedText
   // let shouldUpdate = false
-  const banner = `
-,-.   ,.  ,    ,--. .                
-(   \` /  \ |    |    |                
- \`-.  |--| |    |--  |                
-.   ) |  | |    |                     
- \`-'  '  ' \`--' \`--' o                `
+//   const banner = `
+// ,-.   ,.  ,    ,--. .                
+// (   \` /  \ |    |    |                
+//  \`-.  |--| |    |--  |                
+// .   ) |  | |    |                     
+//  \`-'  '  ' \`--' \`--' o                `
 
-  // const banner = `
-  //  .d8888b.        d8888 888      8888888888      
-  // d88P  Y88b      d88888 888      888             
-  // Y88b.          d88P888 888      888             
-  //  "Y888b.      d88P 888 888      8888888         
-  //     "Y88b.   d88P  888 888      888             
-  //       "888  d88P   888 888      888             
-  // Y88b  d88P d8888888888 888      888             
-  //  "Y8888P" d88P     888 88888888 8888888888      `
+  const banner = `
+   .d8888b.        d8888 888      8888888888      
+  d88P  Y88b      d88888 888      888             
+  Y88b.          d88P888 888      888             
+   "Y888b.      d88P 888 888      8888888         
+      "Y88b.   d88P  888 888      888             
+        "888  d88P   888 888      888             
+  Y88b  d88P d8888888888 888      888             
+   "Y8888P" d88P     888 88888888 8888888888      `
 
   const bannerLines= banner.split('\n')
   const maxFrames = bannerLines[1].length
