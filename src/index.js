79bac69eb6cca1e931bc903e76abfa6cd8afaf6d
Max Wofford <max@maxwofford.com>
2021-08-19 00:28:50 -0400

    Superset of wahoo fish features

--- src/index.js
+++ src/index.js
@@ -106,7 +106,7 @@ controller.hears(
 
 controller.hears(
   ['wahoo', 'fish'],
-  'mention,indirect_mention,direct_mention,direct_message',
+  'mention,indirect_mention,direct_mention,direct_message,ambient',
   (bot, message) => {
     bot.replyInThread(message, transcript("wahooFish"));
   }
