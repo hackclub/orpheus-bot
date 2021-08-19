40b6c63ba77e08f8c971b909474d25e0f688f765
Max Wofford <max@maxwofford.com>
2021-08-18 22:52:00 -0400

    Fix one too many spaces

--- src/interactions/wordcloud.js
+++ src/interactions/wordcloud.js
@@ -52,7 +52,7 @@ const interactionWordcloud = async (bot = initBot(true), message) => {
 //    `
 
   const banner = `
-   .d8888b.        d8888 888      8888888888       
+   .d8888b.        d8888 888      8888888888      
   d88P  Y88b      d88888 888      888             
   Y88b.          d88P888 888      888             
    "Y888b.      d88P 888 888      8888888         
