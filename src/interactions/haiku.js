ad8205bfb9901da512a101bb95188e516b7c4eb7
Max Wofford <max@maxwofford.com>
2022-03-02 16:09:02 -0500

    Add haiku reaction

--- src/interactions/haiku.js
+++ src/interactions/haiku.js
@@ -1,12 +1,13 @@
-import { transcript } from "../utils"
+import { reaction, transcript } from "../utils"
 import { format } from "haiku-detect"
 
-const interactionHaiku = (bot, message) => {
-  const { text, user } = message
+const interactionHaiku = async (bot, message) => {
+  const { text, user, ts } = message
   const noLineText = text.replace(/\n/g, ' ')
   const haikuLines = format(noLineText)
   if (haikuLines.length == 3) {
-    bot.replyInThread(message, transcript('haiku', { haikuLines, user }))
+    await bot.replyInThread(message, transcript('haiku', { haikuLines, user }))
+    await reaction(bot, 'add', channel, ts, 'haiku')
   }
 }
 
