e22a95d2ca8ef450dce5f6dde1b8d1c79d0c47c6
Max Wofford <max@maxwofford.com>
2022-03-02 00:52:33 -0500

    Remove newlines in haiku input

--- src/interactions/haiku.js
+++ src/interactions/haiku.js
@@ -3,7 +3,8 @@ import { format } from "haiku-detect"
 
 const interactionHaiku = (bot, message) => {
   const { text, user } = message
-  const haikuLines = format(text)
+  const noLineText = text.replace(/\n/g, ' ')
+  const haikuLines = format(noLineText)
   if (haikuLines.length == 3) {
     bot.replyInThread(message, transcript('haiku', { haikuLines, user }))
   }
