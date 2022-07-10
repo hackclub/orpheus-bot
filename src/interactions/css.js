8237f09212610b3e120a1e42dd5bbabf84c9704a
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-07-10 16:01:55 -0400

    Push! Push! Push!

--- src/interactions/css.js
+++ src/interactions/css.js
@@ -1,11 +1,24 @@
 import { reaction } from "../utils"
 let validateCss = require('css-validator');
 
+const Pusher = require("pusher");
+
+const pusher = new Pusher({
+  appId: "1435548",
+  key: "de6cd13556d73c05beed",
+  secret: process.env.pusher,
+  cluster: "us2",
+  useTLS: true
+});
+
 const interactionCSS = async (bot, message) => {
   const { text, channel, ts } = message
   validateCss({text}, async function (_, data) {
     if(data.validity){
       await reaction(bot, 'add', channel, ts, 'art')
+      pusher.trigger("css", "new", {
+        message: text
+      });
     }
   });
 }
